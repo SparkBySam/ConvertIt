825ee92236581f11b48988c373f57d67a13dc717
Sam Parker <parkersam1123@gmail.com>
2026-06-06 13:11:12 -0400

    Initial Commit

---
+++ Convert It/Convert_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_ItApp.swift
+//  Convert It
+//
+//  Created by Sam Parker on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Convert_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
